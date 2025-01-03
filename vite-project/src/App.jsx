e745111af383ac539d6cbec9a066e96e76d31ba1
KeilorCruz <cruzkeilor@gmail.com>
2025-01-03 17:51:35 -0600

    se agrega el v login

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -29,7 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p className="read-the-docs">
-        modificacion 1 v2
+        modificacion 1 v login
       </p>
     </>
   )
